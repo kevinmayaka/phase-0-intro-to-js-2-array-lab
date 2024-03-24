077b131def8acfbe28a1666eb94bf9f450f698a3
Kevin Mayaka <kevinabuga3@gmail.com>
2024-03-24 19:56:44 +0300

    Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+// Write your solution h
+let cats = ["Milo", "Otis", "Garfield"];
+
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name, catsArray) {
+    return [...catsArray, name];
+}
+
+function prependCat(name) {
+    const newCatsArray = [...cats];
+    newCatsArray.unshift("name")
+
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+    const newCatsArray = removeFirstCat(cats);
+}
+
+function removeLastCat(catsArray) {
+    return catsArray.slice(0, -1);
+}
+
+
